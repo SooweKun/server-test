2dc8f90a88318b0e3122a2599d215a5c51dca058
SooweKun <apextoplega@mail.ru>
2025-12-23 22:54:47 +0300

    ci/cd test

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='w-full h-screen bg-[#1a1a1a] flex justify-center items-center'>
+        <h1 className='text-[40px] text-fuchsia-300'> Я тебя люблю</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
